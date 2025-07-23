a5fe50619f630972c336390d77c39be04d50ebbe
picospitfire <picospitfire@icloud.com>
2025-07-23 14:39:22 +0200

    Match

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let total = add(10, 15);
+    let mut total = add(10, 15);
+    let mut bonus = false;
 
     if total > 50 {
+        bonus = true;
         println!("You are qualified for the bonus!");
     } else if total > 20 {
         println!("You are qualified for the promotion!");
@@ -13,4 +15,8 @@ fn main() {
         println!("You are not qualified for any bonus or promotion.");
     }
 
+    match bonus {
+        true => println!("Congratulations on your bonus!"),
+        false => println!("Keep up the good work!"),
+    }
 }
