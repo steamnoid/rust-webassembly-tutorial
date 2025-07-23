cab885a0de8a54c8b4bd01fde2f105fabadcca03
picospitfire <picospitfire@icloud.com>
2025-07-23 15:37:05 +0200

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,22 @@ fn print_verified(account: &BankAccount) {
     println!("Verified: {}", account.verified);
 }
 
+fn is_verified(account: &BankAccount) -> Result<bool, bool> {
+   match account.verified {
+        true => Ok(true),
+        false => Err(false),
+    }
+}
+  
 fn main() {
     let my_account = BankAccount {
         balance: 1000,
         verified: true,
     };
 
+    let verification_status = is_verified(&my_account).expect("Account verification check failed");
+
     print_balance(&my_account);
     print_verified(&my_account);
+    println!("Verification Status: {:?}", verification_status);
 }
