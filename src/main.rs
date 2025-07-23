563411aa666a34c5535a25ad5c3869bdc538dccd
picospitfire <picospitfire@icloud.com>
2025-07-23 15:19:12 +0200

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
+  struct BankAccount {
+    balance: i32,
+    verified: bool,
+}
+
 fn main() {
-    let items = [1, 2, 3, 4, 5];
-    println!("Items: {:?}", items);
-
-    let vector_items = vec![1, 2, 3, 4, 5];
-    println!("Vector Items: {:?}", vector_items);
-
-    let mut vector_items_2 = Vec::new();
-    vector_items_2.push(1);
-    vector_items_2.push(2);
-    vector_items_2.push(3);
-    vector_items_2.push(4);
-    vector_items_2.push(5);
-    println!("Vector Items 2: {:?}", vector_items_2);
+    let my_account = BankAccount {
+        balance: 1000,
+        verified: true,
+    };
+    println!("Account Balance: {}", my_account.balance);
+    println!("Account Verified: {}", my_account.verified);
 }
