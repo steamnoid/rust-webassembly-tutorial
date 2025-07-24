be0801a5d13add702e252395e7cf6f296604911f
picospitfire <picospitfire@icloud.com>
2025-07-24 07:21:11 +0200

    File upload

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
 console.log('test');
+
+function init() {
+    const input = document.getElementById('upload');
+    const fileReader = new FileReader();  
+
+    fileReader.onload = (event) => {
+        const base64 = fileReader.result.replace(
+            /^data:.+;base64,/,
+            ''
+        );
+        console.log(input.files[0]);
+        console.log(base64);
+    }
+    input.addEventListener('change', (event) => {
+        fileReader.readAsDataURL(input.files[0])});
+}
+
+init();
